323d774a8b18124d4741731e12c267586d0e656c
Walter <silvawalteralipio@gmail.com>
2024-04-12 22:00:06 -0300

    Third class: import json and filter data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,14 +4,31 @@ import { RouterOutlet } from '@angular/router';
 import { ContainerComponent } from './componentes/container/container.component';
 import { CabecalhoComponent } from './componentes/cabecalho/cabecalho.component';
 import { SeparadorComponent } from './componentes/separador/separador.component';
+import { ContatoComponent } from './componentes/contato/contato.component';
+
+interface Contato {
+  id: number,
+  nome: string,
+  telefone: string
+}
+
+import agenda from './agenda.json' 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, ContainerComponent, CabecalhoComponent, SeparadorComponent],
+  imports: [CommonModule, RouterOutlet, ContainerComponent, CabecalhoComponent, SeparadorComponent, ContatoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  alfabeto: string = 'abcdefghijklmnopqrstuvwxyz'
+  alfabeto: string = 'abcdefghijklmnopqrstuvwxyz';
+  contatos: Contato[] = agenda;
+
+  filtrarContosPorLetraInicial(letra:string) : Contato[]
+  {
+    return this.contatos.filter( contato => {
+      return contato.nome.toLowerCase().startsWith( letra );
+    });
+  }
 }
